e178cdf1560f5b25ba2e9ef6ab90218f143d5632
Felipe Almeida <lipe_fit@hotmail.com>
2020-08-13 19:38:40 -0300

    Excluindo productController

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resource('products', 'ProductController');
+
+/*
 Route::delete('/products/{id}/delete', 'ProductController@destroy')->name('products.destroy');
-Route::put('/products/{id}/update', 'ProductController@update')->name('products.update');
-Route::post('/products/store', 'ProductController@store')->name('products.store');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::post('/products', 'ProductController@store')->name('products.store');
 Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
 Route::get('/products/create', 'ProductController@create')->name('products.create');
 Route::get('/products/{id}', 'ProductController@show')->name('products.show');
 Route::get('/products', 'ProductController@index')->name('products.index');
+*/
 
 Route::get('/admin/login', function () {
     return "Login";
